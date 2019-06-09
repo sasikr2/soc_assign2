12842ce9df2fc5f8c3d37e899661a4fb51474620
sasikr2 <shashikr2201@gmail.com>
2019-06-09 17:47:00 +0530

    statement1

---
+++ 1.js
@@ -0,0 +1,21 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function rightSlides() {
+  showSlides(slideIndex += 1);
+}
+function leftSlides() {
+    showSlides(slideIndex -= 1);
+  }
+
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  slides[slideIndex-1].style.display = "block";
+}
